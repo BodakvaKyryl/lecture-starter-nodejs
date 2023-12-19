f193c8f7821edcd0ea55455d6bbd67c5fa38660e
Bodakva Kyryl <bodakvakyryl@gmail.com>
2023-12-19 13:22:47 +0200

    Implement route controllers for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,85 @@ import { responseMiddleware } from '../middlewares/response.middleware.js'
 
 const router = Router()
 
-// TODO: Implement route controllers for user
+router.get(
+    '/',
+    (req, res, next) => {
+        try {
+            res.data = userService.getAll()
+        } catch (err) {
+            res.err = err
+            res.err.status = 404
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.get(
+    '/:id',
+    (req, res, next) => {
+        try {
+            res.data = userService.getOne(req.params.id)
+        } catch (err) {
+            res.err = err
+            res.err.status = 404
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.post(
+    '/',
+    createUserValid,
+    (req, res, next) => {
+        try {
+            if (!res.err) {
+                res.data = userService.create(req.body)
+            }
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.put(
+    '/:id',
+    updateUserValid,
+    (req, res, next) => {
+        try {
+            if (!res.err) {
+                res.data = userService.update(req.params.id, req.body)
+            }
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.delete(
+    '/:id',
+    (req, res, next) => {
+        try {
+            res.data = userService.delete(req.params.id)
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
 
 export { router }
