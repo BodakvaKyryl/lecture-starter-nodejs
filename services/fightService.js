747c9b01e128b6cd0003717588c2b7b66ce4c91a
Bodakva Kyryl <bodakvakyryl@gmail.com>
2023-12-19 12:07:36 +0200

    Implement methods to work with fights

--- services/fightService.js
+++ services/fightService.js
@@ -1,7 +1,53 @@
 import { fightRepository } from '../repositories/fightRepository.js'
 
 class FightersService {
-    // OPTIONAL TODO: Implement methods to work with fights
+    search(data) {
+        const item = fightRepository.getOne(data)
+        if (!item) {
+            return null
+        }
+        return item
+    }
+
+    getAll() {
+        const items = fightRepository.getAll()
+        if (!items) {
+            return null
+        }
+        return items
+    }
+
+    getOne(id) {
+        const item = fightRepository.getOne(id)
+        if (!item) {
+            throw new Error('Fight not found.')
+        }
+        return item
+    }
+
+    create(data) {
+        const item = fightRepository.create(data)
+        if (!item) {
+            throw new Error('Fight not created.')
+        }
+        return item
+    }
+
+    update(id, data) {
+        const item = fightRepository.update(id, data)
+        if (!item) {
+            throw new Error('Fight not updated.')
+        }
+        return item
+    }
+
+    delete(id) {
+        const item = fightRepository.delete(id)
+        if (!item) {
+            throw new Error('Fight not deleted.')
+        }
+        return item
+    }
 }
 
 const fightersService = new FightersService()
