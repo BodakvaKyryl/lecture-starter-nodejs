f6ba896a97ed4fa1578a8295d76ef6ee58523812
Bodakva Kyryl <bodakvakyryl@gmail.com>
2023-12-19 12:36:12 +0200

    Implemented route controllers for fighter

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -5,6 +5,88 @@ import { createFighterValid, updateFighterValid } from '../middlewares/fighter.v
 
 const router = Router()
 
-// TODO: Implement route controllers for fighter
+router.get(
+    '/',
+    (req, res) => {
+        try {
+            const fighters = fighterService.getAll()
+            res.data = fighters
+        } catch (err) {
+            res.err = err
+            res.err.status = 404
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.get(
+    '/:id',
+    (req, res, next) => {
+        try {
+            const fighter = fighterService.getOne(req.params.id)
+            res.data = fighter
+        } catch (err) {
+            res.err = err
+            res.err.status = 404
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.post(
+    '/',
+    createFighterValid,
+    (req, res, next) => {
+        try {
+            if (!res.err) {
+                res.data = fighterService.create(req.body)
+            }
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.put(
+    '/:id',
+    updateFighterValid,
+    (req, res, next) => {
+        try {
+            if (!res.err) {
+                res.data = fighterService.update(req.params.id, req.body)
+            }
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.delete(
+    '/:id',
+    (req, res, next) => {
+        try {
+            const fighter = fighterService.delete(req.params.id)
+            res.data = fighter
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
 
 export { router }
