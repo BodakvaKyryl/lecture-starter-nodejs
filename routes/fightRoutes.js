02a5c8d0287f820ede87d9f10e630e00a47b87fe
Bodakva Kyryl <bodakvakyryl@gmail.com>
2023-12-19 13:12:56 +0200

    Implement route controller for fights

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -5,6 +5,71 @@ import { responseMiddleware } from '../middlewares/response.middleware.js'
 
 const router = Router()
 
-// OPTIONAL TODO: Implement route controller for fights
+router.get(
+    '/',
+    (req, res, next) => {
+        try {
+            const fights = fightersService.getAll()
+            res.data = fights
+        } catch (err) {
+            res.err = err
+            res.err.status = 404
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.get(
+    '/:id',
+    (req, res, next) => {
+        try {
+            const fight = fightersService.getOne(req.params.id)
+
+            res.data = fight
+        } catch (err) {
+            res.err = err
+            res.err.status = 404
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.post(
+    '/',
+    (req, res, next) => {
+        try {
+            const { fighter1, fighter2 } = req.body
+            res.data = fightersService.create({ fighter1, fighter2 })
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
+
+router.put(
+    '/:id',
+    (req, res, next) => {
+        try {
+            const { log } = req.body
+            res.data = fightersService.update(req.params.id, {
+                log,
+            })
+        } catch (err) {
+            res.err = err
+            res.err.status = 400
+        } finally {
+            next()
+        }
+    },
+    responseMiddleware
+)
 
 export { router }
